be754ed9dd655d3db8b67cf490d4d15f555b9e2a
PhamTuyetAnh <phamtuyetanh2@gmail.com>
2024-06-08 00:59:21 +0700

    Add Fe_design(alphabetlessonContent.html and studyAlphabet.html + css +js)

---
+++ FE_design/assets/js/alphabet.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //alphabet_audio
+    var button_alphabet = document.querySelectorAll('.button_alphabet');
+    for (var i = 0; i < button_alphabet.length; i++) {
+        button_alphabet[i].onclick = function () {
+            var audio = document.querySelector('.audios');
+            var source = document.querySelector('source')
+            source.src = this.getAttribute('data-srcAudio')
+            console.log(source);
+            audio.load();
+            audio.play();
+
+        }
+    }
+    // alphabet_flashcard
+    // alphabet_flashcard right
+    var clickRight = document.querySelector('i.fa-solid.fa-chevron-right');
+    var flashcards = document.querySelectorAll('.flashcards_alphabet .flashcard');
+    var numberFlashcardsNow = 0;
+    var numberFlashcards = flashcards.length;
+    clickRight.onclick = function() {
+        var flashcardNow = flashcards[numberFlashcardsNow];
+        console.log(flashcardNow);
+        if (numberFlashcardsNow < numberFlashcards - 1) {
+            numberFlashcardsNow++;
+        }else{
+            numberFlashcardsNow = 0
+        }
+        var flashcardNext = flashcards[numberFlashcardsNow];
+        flashcardNow.classList.remove('active');
+        flashcardNext.classList.add('active');
+    }
+    // alphabet_flashcard right
+    var clickLeft = document.querySelector('i.fa-solid.fa-chevron-left');
+    clickLeft.onclick = function() {
+        var flashcardNow = flashcards[numberFlashcardsNow];
+        console.log(flashcardNow);
+        if (numberFlashcardsNow > 0) {
+            numberFlashcardsNow --;
+        }else{
+            numberFlashcardsNow = numberFlashcards - 1
+        }
+        var flashcardNext = flashcards[numberFlashcardsNow];
+        flashcardNow.classList.remove('active');
+        flashcardNext.classList.add('active');
+    }
+})
