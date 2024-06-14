71990f5265d477c6eed2c5ac0876e0bec2dff2ae
PhamTuyetAnh <phamtuyetanh2@gmail.com>
2024-06-14 23:58:46 +0700

    add chatbot.js and StudyCommunicationUser.js

--- FE_ReactJS/client/src/component/communication/ChatBots.js
+++ FE_ReactJS/client/src/component/communication/ChatBots.js
@@ -5,10 +5,10 @@ class ChatBots extends Component {
     render() {
         return (
                 <ChatBot
-                    // headerTitle="Luyện shadowing nào"
-                    // speechSynthesis={{ enable: true, lang: 'ja' }}
-                    // recognitionEnable={true}
-                    // recognitionLang="ja"
+                    headerTitle="Luyện shadowing nào"
+                    speechSynthesis={{ enable: true, lang: 'ja' }}
+                    recognitionEnable={true}
+                    recognitionLang="ja"
                     steps={[
                         {
                             id: 'hello',
@@ -22,107 +22,107 @@ class ChatBots extends Component {
                                     <source src={'/sound/'+ this.props.sound_shadowing} type="audio/mp3" />
                                 </audio>
                             ),
-                            trigger: 'notice',
+                            trigger: 'note',
                         },
-                        // {
-                        //     id: 'note',
-                        //     component: (
-                        //         <h5>Hãy nghe nhiều lần và luyện tập</h5>
-                        //     ),
-                        //     trigger: 'one_a',
-                        // },
-                        // {
-                        //     id: 'one_a',
-                        //     message: this.props.one_a,
-                        //     trigger: 'one_b_write', 
-                        // },
-                        // {
-                        //     id: 'one_b_write',
-                        //     user: true,
-                        //     trigger: 'one_b',
-                        // },
-                        // {
-                        //     id: 'one_b',
-                        //     component: (
-                        //         <div>Đáp án: {this.props.one_b}</div>
-                        //     ),
-                        //     trigger: 'two_a',
-                        // },
-                        // {
-                        //     id: 'two_a',
-                        //     message: this.props.two_a,
-                        //     trigger: 'two_b_write', 
-                        // },
-                        // {
-                        //     id: 'two_b_write',
-                        //     user: true,
-                        //     trigger: 'two_b',
-                        // },
-                        // {
-                        //     id: 'two_b',
-                        //     component: (
-                        //         <div>Đáp án: {this.props.two_b}</div>
-                        //     ),
-                        //     trigger: 'three_a',
-                        // },
-                        // {
-                        //     id: 'three_a',
-                        //     message: this.props.three_a,
-                        //     trigger: 'three_b_write', 
-                        // },
-                        // {
-                        //     id: 'three_b_write',
-                        //     user: true,
-                        //     trigger: 'three_b',
-                        // },
-                        // {
-                        //     id: 'three_b',
-                        //     component: (
-                        //         <div>Đáp án: {this.props.three_b}</div>
-                        //     ),
-                        //     trigger: 'four_a',
-                        // },
-                        // {
-                        //     id: 'four_a',
-                        //     message: this.props.four_a,
-                        //     trigger: 'four_b_write', 
-                        // },
-                        // {
-                        //     id: 'four_b_write',
-                        //     user: true,
-                        //     trigger: 'four_b',
-                        // },
-                        // {
-                        //     id: 'four_b',
-                        //     component: (
-                        //         <div>Đáp án: {this.props.four_b}</div>
-                        //     ),
-                        //     trigger: 'five_a',
-                        // },
-                        // {
-                        //     id: 'five_a',
-                        //     message: this.props.five_a,
-                        //     trigger: 'five_b_write', 
-                        // },
-                        // {
-                        //     id: 'five_b_write',
-                        //     user: true,
-                        //     trigger: 'five_b',
-                        // },
-                        // {
-                        //     id: 'five_b',
-                        //     component: (
-                        //         <div>Đáp án: {this.props.five_b}</div>
-                        //     ),
-                        //     trigger: 'mean',
-                        // },
-                        // {
-                        //     id: 'mean',
-                        //     component: (
-                        //         <div>Nghĩa: {this.props.mean_shadowing}</div>
-                        //     ),
-                        //     trigger:'notice'
-                        // },
+                        {
+                            id: 'note',
+                            component: (
+                                <h5>Hãy nghe nhiều lần và luyện tập</h5>
+                            ),
+                            trigger: 'one_a',
+                        },
+                        {
+                            id: 'one_a',
+                            message: this.props.one_a,
+                            trigger: 'one_b_write', 
+                        },
+                        {
+                            id: 'one_b_write',
+                            user: true,
+                            trigger: 'one_b',
+                        },
+                        {
+                            id: 'one_b',
+                            component: (
+                                <div>Đáp án: {this.props.one_b}</div>
+                            ),
+                            trigger: 'two_a',
+                        },
+                        {
+                            id: 'two_a',
+                            message: this.props.two_a,
+                            trigger: 'two_b_write', 
+                        },
+                        {
+                            id: 'two_b_write',
+                            user: true,
+                            trigger: 'two_b',
+                        },
+                        {
+                            id: 'two_b',
+                            component: (
+                                <div>Đáp án: {this.props.two_b}</div>
+                            ),
+                            trigger: 'three_a',
+                        },
+                        {
+                            id: 'three_a',
+                            message: this.props.three_a,
+                            trigger: 'three_b_write', 
+                        },
+                        {
+                            id: 'three_b_write',
+                            user: true,
+                            trigger: 'three_b',
+                        },
+                        {
+                            id: 'three_b',
+                            component: (
+                                <div>Đáp án: {this.props.three_b}</div>
+                            ),
+                            trigger: 'four_a',
+                        },
+                        {
+                            id: 'four_a',
+                            message: this.props.four_a,
+                            trigger: 'four_b_write', 
+                        },
+                        {
+                            id: 'four_b_write',
+                            user: true,
+                            trigger: 'four_b',
+                        },
+                        {
+                            id: 'four_b',
+                            component: (
+                                <div>Đáp án: {this.props.four_b}</div>
+                            ),
+                            trigger: 'five_a',
+                        },
+                        {
+                            id: 'five_a',
+                            message: this.props.five_a,
+                            trigger: 'five_b_write', 
+                        },
+                        {
+                            id: 'five_b_write',
+                            user: true,
+                            trigger: 'five_b',
+                        },
+                        {
+                            id: 'five_b',
+                            component: (
+                                <div>Đáp án: {this.props.five_b}</div>
+                            ),
+                            trigger: 'mean',
+                        },
+                        {
+                            id: 'mean',
+                            component: (
+                                <div>Nghĩa: {this.props.mean_shadowing}</div>
+                            ),
+                            trigger:'notice'
+                        },
                         {
                             id: 'notice',
                             component: (
